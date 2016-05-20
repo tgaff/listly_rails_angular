8b1a9dbd20d918e78ec399096c4354dad1cb3ec3
tgaff <tgaff@alumni.nd.edu>
2016-05-20 01:03:21 -0700

    add root route and catchall route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'site/angular'
-
+  root 'site#angular'
   # api/ urls and default to json
   scope '/api', defaults: { format: :json } do
     # since we're just doing a json api we don't need new and edit pages to hold forms for our users
@@ -9,4 +8,8 @@ Rails.application.routes.draw do
       resources :items, except: [:new, :edit]
     end
   end
+
+  # route all other requests to site#angular
+  # our front-end router will then determine what to really display for that URL
+  get '*path', to: 'site#angular'
 end
