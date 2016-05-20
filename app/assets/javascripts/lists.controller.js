889afc5fc1d7fa00c755b8caace42b16d86b18fb
tgaff <tgaff@alumni.nd.edu>
2016-05-20 16:11:34 -0700

    angular loads data onto the page and uses templates

--- app/assets/javascripts/lists.controller.js
+++ app/assets/javascripts/lists.controller.js
@@ -3,8 +3,13 @@ angular
   .controller('ListsController', ListsController);
 
 ListsController.$inject = [];
-function ListsController() {
+function ListsController(      ) {
   var vm = this;
   console.log('ListsController is live');
   vm.lists = [{"id":1,"name":"hi","url":"http://localhost:3000/api/lists/1"},{"id":2,"name":"sample list","url":"http://localhost:3000/api/lists/2"},{"id":3,"name":"sample list","url":"http://localhost:3000/api/lists/3"},{"id":4,"name":"student list","url":"http://localhost:3000/api/lists/4"}];
+  vm.toggleEditForm = toggleEditForm;
+
+  function toggleEditForm(list) {
+    list.showForm = !list.showForm;
+  }
 }
