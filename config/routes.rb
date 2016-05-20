d3aa838c6e2dbf06f7083ed81ecb486f9eabaabb
tgaff <tgaff@alumni.nd.edu>
2016-05-20 00:59:09 -0700

    rails g controller Site angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'site/angular'
+
   # api/ urls and default to json
   scope '/api', defaults: { format: :json } do
     # since we're just doing a json api we don't need new and edit pages to hold forms for our users
