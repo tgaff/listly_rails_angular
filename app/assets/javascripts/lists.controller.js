f0b80d15298707bbf1a80faa98bc7f84042a024d
tgaff <tgaff@alumni.nd.edu>
2016-05-21 15:59:19 -0700

    implement create for lists

--- app/assets/javascripts/lists.controller.js
+++ app/assets/javascripts/lists.controller.js
@@ -7,8 +7,10 @@ function ListsController(   ListsService    ) {
   var vm = this;
   console.log('ListsController is live');
   vm.lists = [];
+  vm.newListName = '';
   vm.toggleEditForm = toggleEditForm;
   vm.deleteList = deleteList;
+  vm.createList = createList;
   // fetch data
   getLists();
 
@@ -26,10 +28,22 @@ function ListsController(   ListsService    ) {
   function deleteList(list) {
     ListsService.remove({id: list.id}, handleDeleteSuccess);
 
+      // declaring this inside deleteList to have a closure around list variable
       function handleDeleteSuccess(data) {
         console.log('deleted');
         vm.lists.splice(vm.lists.indexOf(list), 1);
       }
   }
 
+  function createList() {
+    console.log('create with', vm.newListName);
+    if(vm.newListName.length > 1) {
+      ListsService.save({name: vm.newListName}, handleCreateSuccess);
+      vm.newListName = '';
+    }
+  }
+  function handleCreateSuccess(data) {
+    console.log('created', data);
+    vm.lists.unshift(data);
+  }
 }
